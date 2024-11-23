69a0761a80319d92bfbad627790520ba8cf04119
mathias <pfannenkuchen107@gmx.de>
2024-11-23 22:11:30 +0100

    Commit to push my stats

--- src/memory.rs
+++ src/memory.rs
@@ -14,4 +14,12 @@ impl Memory {
     pub fn read(&mut self, address: u16) -> u8 {
         self.data[address as usize]
     }
+
+    pub fn write(&mut self, address: u16, value: u8) {
+        self.data[address as usize] = value;
+    }
+
+    pub fn size(&mut self) -> usize {
+        self.data.len()
+    }
 }
