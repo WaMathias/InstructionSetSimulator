73dd073753137fd73ca31ba94d1bc60c4315092a
mathias <pfannenkuchen107@gmx.de>
2024-11-25 22:04:06 +0100

    Commit to push my stats

--- src/memory.rs
+++ src/memory.rs
@@ -18,6 +18,29 @@ impl Memory {
     pub fn write(&mut self, address: usize, value: u8) {
         self.memory[address] = value;
     }
+
+    pub fn init_memory(&mut self, size: usize, default_value: u8) -> Vec<u8> {
+        vec![default_value; size]
+    }
+    pub fn clear(&mut self, default_value: u8) {
+        for byte in self.memory.iter_mut() {
+            *byte = default_value;
+        }
+    }
+
+    pub fn dump(&mut self) {
+        for (i, byte) in self.memory.iter().enumerate() {
+            println!("Address: {}: {}", i, byte);
+        }
+    }
+
+    pub fn copy(&mut self, src: usize, dest: usize, length: usize) {
+        for i in 0..length {
+            self.memory[dest + i] = self.memory[src + i];
+        }
+    }
+
+    //fn compare(&mut self, )
 }
 
 // TODO: Speicherinitialiserung, Daten lesen, Daten schreiben, Speicher löschen, Speicher Dump, Speicherbereich kopieren, Speicher vergleichen, Blcokweises Laden, Speicherbereich schützen, Speichergröße anpassen, Speichersperren,
